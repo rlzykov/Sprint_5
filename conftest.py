ec525ddbe49d856f9d265b77aaf79b70a88add3e
Roman Zykov <zykovrl@yandex.ru>
2024-03-18 19:25:05 +0300

    убрал ретурн

--- conftest.py
+++ conftest.py
@@ -19,5 +19,3 @@ def get_login_driver(driver):
     driver.find_element(*AuthPageLocators.email_input).send_keys(UserProfile.email)
     driver.find_element(*AuthPageLocators.password_input).send_keys(UserProfile.password)
     driver.find_element(*AuthPageLocators.login_account_btn).click()
-
-    return driver
