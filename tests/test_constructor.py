77c0e7458937346bb2305b713a740990702dd660
Roman Zykov <zykovrl@yandex.ru>
2024-03-20 02:08:44 +0300

    изменен код (новый класс)

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -8,25 +8,19 @@ class TestConstructorPage:
         driver.get(LinksStorage.MAIN_PAGE_URL)
         driver.find_element(*MainPageLocators.sauces_btn).click()
         driver.find_element(*MainPageLocators.bun_btn).click()
-        bun_text = driver.find_element(*MainPageLocators.bun).text
-        bun_displayed = driver.find_element(*MainPageLocators.bun_ul).is_displayed()
-
-        assert bun_text == 'Булки' and bun_displayed
+        class_bun_ul = driver.find_element(*MainPageLocators.bun_ul).get_attribute('class')
+        assert 'tab_tab_type' in class_bun_ul
 
     def test_transition_to_sauces_success(self, driver):
         """Проверка перехода к разделу 'Соусы' """
         driver.get(LinksStorage.MAIN_PAGE_URL)
         driver.find_element(*MainPageLocators.sauces_btn).click()
-        souces = driver.find_element(*MainPageLocators.sauces).text
-        souces_displayed = driver.find_element(*MainPageLocators.sauces_ul).is_displayed()
-
-        assert souces == 'Соусы' and souces_displayed
+        class_sauces_ul = driver.find_element(*MainPageLocators.sauces_ul).get_attribute('class')
+        assert 'tab_tab_type' in class_sauces_ul
 
     def test_transition_to_topping_success(self, driver):
         """Проверка перехода к разделу 'Начинки' """
         driver.get(LinksStorage.MAIN_PAGE_URL)
         driver.find_element(*MainPageLocators.toppings_btn).click()
-        topping = driver.find_element(*MainPageLocators.topping).text
-        topping_displayed = driver.find_element(*MainPageLocators.topping_ul).is_displayed()
-
-        assert topping == 'Начинки' and topping_displayed
+        class_topping_ul = driver.find_element(*MainPageLocators.topping_ul).get_attribute('class')
+        assert 'tab_tab_type' in class_topping_ul
