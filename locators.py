41ca487ee8f1616cd84382eac35c30955e948f91
Roman Zykov <zykovrl@yandex.ru>
2024-03-20 02:02:23 +0300

    изменен код с порядковыми номерами

--- locators.py
+++ locators.py
@@ -11,20 +11,20 @@ class MainPageLocators:
     order_feed_btn = (By.XPATH, ".//p[text() = 'Лента Заказов']")  # Кнопка лента заказов
     bun_btn = (By.XPATH, ".//span[text() = 'Булки']")  # Кнопка переключения на булки
     sauces_btn = (By.XPATH, ".//span[text() = 'Соусы']")  # Кнопка переключения на соусы
-    toppings_btn = toppings_btn = (By.XPATH, ".//span[text() = 'Начинки']")  # Кнопка переключения на начинки
+    toppings_btn = (By.XPATH, ".//span[text() = 'Начинки']")  # Кнопка переключения на начинки
     place_order_button = (By.XPATH, ".//button[text() = 'Оформить заказ']")  # Кнопка оформить заказ
     sauces = (By.XPATH, ".//h2[text() = 'Соусы']")  # Текст соусы на главной странице
-    sauces_ul = (
-        By.XPATH,
-        ".//h2[text() = 'Соусы']/following-sibling::ul[@class = 'BurgerIngredients_ingredients__list__2A-mT']")  # Выбор соусов на главной странице
+    sauces_ul = (By.XPATH,
+                 ".//span[(@class = 'text text_type_main-default' and text()='Соусы')]/parent::div")  # Выбор соусов
+    # на главной странице
     bun = (By.XPATH, ".//h2[text() = 'Булки']")  # Текст булки на главной странице
-    bun_ul = (
-        By.XPATH,
-        ".//h2[text() = 'Булки']/following-sibling::ul[@class = 'BurgerIngredients_ingredients__list__2A-mT']")  # Выбор булок на главной странице
+    bun_ul = (By.XPATH,
+              ".//span[(@class = 'text text_type_main-default' and text()='Булки')]/parent::div")  # Выбор булок на
+    # главной странице
     topping = (By.XPATH, ".//h2[text() = 'Начинки']")  # Текст начинки на главной странице
-    topping_ul = (
-        By.XPATH,
-        ".//h2[text() = 'Начинки']/following-sibling::ul[@class = 'BurgerIngredients_ingredients__list__2A-mT']")  # Выбор начинок на главной странице
+    topping_ul = (By.XPATH,
+                  ".//span[(@class = 'text text_type_main-default' and text()='Начинки')]/parent::div")  # Выбор
+    # начинок на главной странице
 
 
 class AuthPageLocators:
@@ -45,7 +45,8 @@ class RegistrationPageLocators:
     """Форма регистрации"""
     name_input = (By.XPATH, ".//label[text()='Имя']/following-sibling::input[@name='name']")  # Поле ввода имени
     email_input = (By.XPATH, ".//label[text()='Email']/following-sibling::input[@name='name']")  # Поле ввода email
-    password_input = (By.XPATH, ".//label[text()='Пароль']/following-sibling::input[@name='Пароль']")  # Поле ввода пароля
+    password_input = (By.XPATH, ".//label[text()='Пароль']/following-sibling::input[@name='Пароль']")  # Поле ввода
+    # пароля
     registration_btn = (By.XPATH, ".//button[text() = 'Зарегистрироваться']")  # Кнопка зерегистрироваться
     login_account_btn = (By.XPATH, ".//a[text() = 'Войти']")  # Кнопка войти
     constructor_btn = (By.XPATH, ".//p[text() = 'Конструктор']")  # Кнопка конструктор
